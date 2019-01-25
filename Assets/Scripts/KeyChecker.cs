92f42d8bfed4ac3069d225da9526c5a232b8909f
Javier Riera Chirivella <mrvizious@gmail.com>
2019-01-25 22:25:48 +0100

    Script de KeyChecker y LevelController creados

---
+++ Assets/Scripts/KeyChecker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class KeyChecker : MonoBehaviour
+{
+    [SerializeField]
+    private char c;
+    
+    public void Update(){
+            if(Input.anyKeyDown){
+                Debug.Log(Input.inputString);
+            }
+    }
+}
---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    [System.Serializable]
+    public class TrackedGameObject{
+        public GameObject trackedGameObject;
+        public int x;
+        public int y;
+    }
+
+    public List<TrackedGameObject> gameObjects;
+}
