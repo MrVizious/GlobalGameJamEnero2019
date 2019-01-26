dd15ec375cedc870fc4df1eae0b7b3c76478e1ec
Javier Riera Chirivella <mrvizious@gmail.com>
2019-01-26 01:19:02 +0100

    Movimiento empezado

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -11,7 +11,12 @@ public class LevelController : MonoBehaviour
         public int y;
     }
 
+    //[SerializeField]
+    public List<string> listChars;
     public List<TrackedGameObject> trackedGameObjects;
+    public TrackedGameObject playerPrefab;
+
+    private GameObject player;
 
     void Start(){
         InstantiateObjects();
@@ -19,8 +24,15 @@ public class LevelController : MonoBehaviour
 
 
     private void InstantiateObjects(){
+        player = Instantiate(playerPrefab.trackedGameObject);
+        //CUIDADO TODO Esto utiliza los prefabs en lugar de los objetos creados.
+        //Da igual porque en principio no se van a manipular las posiciones de los prefabs, pero hay que ir con ojo
         foreach(TrackedGameObject o in trackedGameObjects){
             Instantiate(o.trackedGameObject, new Vector3(o.x, o.y, 0), Quaternion.identity);
         }
+    }
+
+    public void StartMovement(){
+        player.GetComponent<CharacterMovement>().StartMovement(listChars);
     }
 }
