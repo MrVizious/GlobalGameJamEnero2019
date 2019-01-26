51935091a373bf11525d50d0460dcff1825b6779
Javier Riera Chirivella <mrvizious@gmail.com>
2019-01-26 06:30:47 +0100

    Pre-dormir que hay sueñito

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -4,26 +4,45 @@ using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
-    [System.Serializable]
-    public class TrackedGameObject{
-        public GameObject trackedGameObject;
-        public int x;
-        public int y;
+
+    //ESTABAS CREANDO EL ARRAY DE MAPOBJECTS CON EL QUE SE DETERMINARÁN LAS COLISIONES Y LOS OBJETOS COLINDANTES
+
+    public int NUMBER_OF_ROWS;
+    public int NUMBER_OF_COLUMNS;
+    //Temporal para Debug
+    public GameObject pistaVisual;
+
+    private class MapObject{
+        public boolean obstacle;
+        public GameObject mapObject;
+
+        public MapObject(boolean isObstacle){
+            obstacle = isObstacle;
+            mapObject = null;
+        }
+        public MapObject(boolean isObstacle, GameObject newObject){
+            obstacle = isObstacle;
+            mapObject = newObject;
+        }
     }
 
-    //[SerializeField]
+
+
+    private MapObject[][] mapa = new MapObject[NUMBER_OF_COLUMNS][NUMBER_OF_ROWS];
+    private List<int> obstacles;
     public int NUMBER_OF_INPUTS;
     public List<string> listChars;
-    public List<TrackedGameObject> trackedGameObjects;
-    public TrackedGameObject playerPrefab;
+    public GameObject playerPrefab;
     public GameObject inputVisualizer;
     private GameObject player;
 
     void Start(){
-        listChars = new List<string>();
-        InstantiateObjects();
-        CreateLetterHolders();
+        //TODO extraer esto para usarlo bien en otros niveles
+        obstacles = new List<int>(new int[] {1,3,5,9,10,14,15,19,20,24,26,28})
 
+        listChars = new List<string>();
+        CreateLetterHolders();
+        player = Instantiate(playerPrefab);
     }
 
     private void CreateLetterHolders(){
@@ -31,7 +50,6 @@ public class LevelController : MonoBehaviour
     }
 
     public void AddLetter(string s){
-        //TODO Letra temporal, hay que cambiarlo por la de verdad
         if(listChars.Count < NUMBER_OF_INPUTS){
             inputVisualizer.GetComponent<InputVisualizer>().AddLetter(s);
             listChars.Add(s);
@@ -40,38 +58,26 @@ public class LevelController : MonoBehaviour
         }
     }
 
-    private void InstantiateObjects(){
-        player = Instantiate(playerPrefab.trackedGameObject);
-        //CUIDADO TODO Esto utiliza los prefabs en lugar de los objetos creados.
-        //Da igual porque en principio no se van a manipular las posiciones de los prefabs, pero hay que ir con ojo
-        foreach(TrackedGameObject o in trackedGameObjects){
-            Instantiate(o.trackedGameObject, new Vector3(o.x, o.y, 0), Quaternion.identity);
-        }
-    }
-
+    //Simplemente empieza la co-rutina de SequentialActions
     public void StartActions(){
         StartCoroutine(SequentialActions());
-
-
     }
 
+    //Ejecuta cada acción del script CharacterController y pausa un segundo antes de ejecutar la siguiente
     IEnumerator SequentialActions()
      {
-
          if(listChars.Count == NUMBER_OF_INPUTS){
              foreach(string s in listChars){
-                 player.GetComponent<CharacterMovement>().Action(s);
+                 player.GetComponent<CharacterAction>().Action(s);
                  yield return new WaitForSeconds(1f);
              }
          }
-         /*
-         iTween.MoveTo(tile.gameObject, iTween.Hash("position", destination1, "time", 2f));
-
-
-         iTween.MoveTo(tile.gameObject, iTween.Hash("position", destination2, "time", 2f));
-         yield return new WaitForSeconds(2f);*/
-
-         // ... other sequential actions here?
      }
 
+     private void CreateWalls(){
+         foreach(int i in obstacles){
+             mapa[i/NUMBER_OF_COLUMNS][i%NUMBER_OF_COLUMNS] = new MapObject(true);
+             Instantiate(pistaVisual, New Vector3(i%NUMBER_OF_COLUMNS, i/NUMBER_OF_COLUMNS, 0), Quaternion.identity);
+         }
+     }
 }
